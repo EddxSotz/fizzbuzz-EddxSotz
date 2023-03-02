29440e495cccba47ad0d6ea603c55387d7267ff0
EddxSotz <96507617+EddxSotz@users.noreply.github.com>
2023-03-02 12:20:01 -0600

    Modified program

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,17 +1,15 @@
 module.exports = function () {
   let arr=[];
  
-  for(let i=0; i<100; i++){
+  for(let i=1; i<=100; i++){
     if(i%3==0 && i%5==0){
       arr.push('FizzBuzz');
     }else if(i%5==0){
       arr.push('Buzz');
     }else if(i%3==0){
       arr.push('Fizz');
-    }else{
-      arr.push(i.toString());
-    }
-     
+    }else
+      arr.push(i.toString());  
   }
   return arr;
  console.log(arr);
