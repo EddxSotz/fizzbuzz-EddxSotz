2be4eaa4c259e218fed458e2916c3316d2f029d7
EddxSotz <96507617+EddxSotz@users.noreply.github.com>
2023-03-02 10:13:37 -0600

    Completed challenge

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,18 @@
 module.exports = function () {
-  // write your code here
+  let arr=[];
+ 
+  for(let i=0; i<100; i++){
+    if(i%3==0 && i%5==0){
+      arr.push('FizzBuzz');
+    }else if(i%5==0){
+      arr.push('Buzz');
+    }else if(i%3==0){
+      arr.push('Fizz');
+    }else{
+      arr.push(i.toString());
+    }
+     
+  }
+  return arr;
+ console.log(arr);
 }
